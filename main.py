0fb417f23402870b7266fcc5a6631e5251f83e62
Ch1cK3N_t4c0s <51533784+jaredrsommer@users.noreply.github.com>
2023-01-28 12:07:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import asyncio
-
+import climage
 from src.bot import Bot 
 
-
+splash = climage.convert('skip.png', width=120, is_unicode=True)
+print(splash)
+# print("Turning on the Money printer...")
+# print("||====================================================================||")
+# print("||--$--------------------------------------------------------------$--||")
+# print("||(100)==================|  SKIP MONEY PRINTER  |================(100)||")
+# print("||>>|          ~         '------========--------'                  |<<||")
+# print("||<<|         /$\              // ____ \\                          |>>||")
+# print("||>>|  12    //L\\            // ///..) \\         L38036133B   12 |<<||")
+# print("||<<|        \\ //           || <||  >|  ||                        |>>||")
+# print("||>>|         \$/            ||  $$ --/  ||        One Hundred     |<<||")
+# print("||<<|      L38036133B        *\\  |__/  //* series                 |>>||")
+# print("||>>|  12                     *\\/___\_//*   1989                  |<<||")
+# print("||<<|      Treasurer     ______/MEV BOTS\________  Secretary 12    |>>||")
+# print("||>>|                 ~| TO SKIP OR NOT TO SKIP |~                 |<<||")
+# print("||(100)===================  BRRRRRRRRRRRRRRRRRR =================(100)||")
+# print("||--$--------------------------------------------------------------$--||")
+# print("||====================================================================||")
 """#############################################"""
 """@USER TODO: CHOOSE ENVIRONMENT VARIABLES PATH"""
 ENV_FILE_PATH = "envs/juno.env"
